c46435924cc5df62ddc40bc32e3f1ef97243f6e9
Jake Soenneker <jake@soenneker.com>
2026-02-04 15:35:31 -0600

    Initial commit

---
+++ test/Soenneker.Queues.Intrusive.ValueMpsc.Tests/ValueIntrusiveMpscQueueTests.cs
@@ -0,0 +1,13 @@
+using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Queues.Intrusive.ValueMpsc.Tests;
+
+public sealed class ValueIntrusiveMpscQueueTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
